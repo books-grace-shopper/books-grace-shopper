43dbe5b48294afe624e952dbec39d18bd7160792
Joshua Hoeflich <joshuahoeflich@gmail.com>
2019-09-17 14:49:10 -0500

    FEATURE: AllBooks page.

--- client/components/AllViews/AllBooks.js
+++ client/components/AllViews/AllBooks.js
@@ -2,16 +2,43 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchBooks} from '../../store/allBooks.js';
 
+/* SINGLE BOOK CARD ON THE PAGE */
+function SingleBook(props) {
+  const book = props.book;
+  return (
+    <div key={book.id}>
+      <hr />
+      <h1>{book.title}</h1>
+      <img style={{height: '200px', width: '200px'}} src={book.imageUrl} />
+      <h2>{book.author}</h2>
+      <h3>${book.price}</h3>
+    </div>
+  );
+}
+
+/* MAP THROUGH ALL THE BOOKS AND GET SINGLE BOOK CARDS */
+function MapBooks(props) {
+  return props.books.map(book => {
+    return <SingleBook key={book.id} book={book} />;
+  });
+}
+
 class AllBooks extends React.Component {
-  async componentDidMount() {
-    console.log(this.props);
-    await this.props.fetchBooks();
+  componentDidMount() {
+    this.props.fetchBooks();
   }
   render() {
+    console.log('THE PROPS ON ALL BOOKS', this.props);
     return (
       <div>
         <h1>All Books</h1>
-        <div>{/*Some kind of map in here*/}</div>
+        <div>
+          {this.props.books ? (
+            <MapBooks books={this.props.books} />
+          ) : (
+            <p>Loading</p>
+          )}
+        </div>
       </div>
     );
   }
