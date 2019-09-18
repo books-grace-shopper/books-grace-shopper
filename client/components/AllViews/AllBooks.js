8a7a41ba3aa8c8f06c9c3919cb9e10cef279c9bb
Daniel Fontana <dfont110@gmail.com>
2019-09-18 13:39:27 -0500

    FEATURE: integrated react-bootstrap, allbooks view. TODO: SelectedBook front and back end

--- client/components/AllViews/AllBooks.js
+++ client/components/AllViews/AllBooks.js
@@ -1,61 +1,78 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {fetchBooks} from '../../store/allBooks.js';
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchBooks} from '../../store/allBooks.js'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
 
 /* SINGLE BOOK CARD ON THE PAGE */
 function SingleBook(props) {
-  const book = props.book;
+  const book = props.book
   return (
-    <div key={book.id}>
+    <>
       <hr />
-      <h1>{book.title}</h1>
-      <img style={{height: '200px', width: '200px'}} src={book.imageUrl} />
-      <h2>{book.author}</h2>
-      <h3>${book.price}</h3>
-    </div>
-  );
+      <div key={book.id} className="single-book-card">
+        <Card style={{width: '16rem'}}>
+          <Card.Img variant="top" src={book.imageUrl} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>{book.author}</Card.Text>
+            <Card.Text>Book Rating</Card.Text>
+            <Card.Text>${book.price}</Card.Text>
+          </Card.Body>
+        </Card>
+        {/* <hr />
+        <img style={{height: '75px', width: '75px'}} src={book.imageUrl} />
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <p>Book rating</p>
+        <p>${book.price}</p> */}
+      </div>
+    </>
+  )
 }
 
 /* MAP THROUGH ALL THE BOOKS AND GET SINGLE BOOK CARDS */
 function MapBooks(props) {
   return props.books.map(book => {
-    return <SingleBook key={book.id} book={book} />;
-  });
+    return <SingleBook key={book.id} book={book} />
+  })
 }
 
 class AllBooks extends React.Component {
   componentDidMount() {
-    this.props.fetchBooks();
+    this.props.fetchBooks()
   }
   render() {
-    console.log('THE PROPS ON ALL BOOKS', this.props);
+    console.log('THE PROPS ON ALL BOOKS', this.props)
     return (
-      <div>
+      // <div className="all-books-container">
+      <>
         <h1>All Books</h1>
-        <div>
+        <div className="all-book-cards">
           {this.props.books ? (
             <MapBooks books={this.props.books} />
           ) : (
             <p>Loading</p>
           )}
         </div>
-      </div>
-    );
+      </>
+      // </div>
+    )
   }
 }
 
 function mapState(state) {
   return {
     books: state.books
-  };
+  }
 }
 
 function mapDispatch(dispatch) {
   return {
     fetchBooks: function() {
-      dispatch(fetchBooks());
+      dispatch(fetchBooks())
     }
-  };
+  }
 }
 
-export default connect(mapState, mapDispatch)(AllBooks);
+export default connect(mapState, mapDispatch)(AllBooks)
