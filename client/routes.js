0027496ebbc7706e28b4ce71b93c89c5cae0cb49
Christine Vargas <christineandreavargas@gmail.com>
2019-09-18 15:20:01 -0500

    TODO: Cart component

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, AllBooks, SelectedBook} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  AllBooks,
+  SelectedBook,
+  Cart
+} from './components'
 import {me} from './store'
 
 /**
@@ -23,6 +30,7 @@ class Routes extends Component {
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/books/:bookId" component={SelectedBook} />
+        <Route exact path="/cart" component={Cart} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
