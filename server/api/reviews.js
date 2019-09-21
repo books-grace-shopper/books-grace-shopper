280cc82b5bad8319c58cc8f783b52f0dc3b638e9
Jake Loew <jakeloew1@gmail.com>
2019-09-21 13:39:01 -0500

    FEATURE: user can post and delete review

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,6 +1,16 @@
 const router = require('express').Router()
 const {Review, Book} = require('../db/models')
 
+router.post('/', async (req, res, next) => {
+  try {
+    const review = await Review.create(req.body)
+    review.dataValues.user = await review.getUser()
+    res.send(review)
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.delete('/:reviewId', async (req, res, next) => {
   try {
     const review = await Review.findByPk(req.params.reviewId)
